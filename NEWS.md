e2b01b50851da19f11cb827278441d478f9e322d
Tan-DESKPC <tan@tanho.ca>
2021-08-21 13:00:14 -0400

    bumpver

--- NEWS.md
+++ NEWS.md
@@ -5,3 +5,4 @@ The `{tantastic}` package is a fresh start on the previous `{tanmisc}` package.
 - Added a `NEWS.md` file to track changes to the package. (v0.1.0)
 - Added `theme_tantastic()` - a riff on `hrbrthemes::theme_modern_rc()` - as well as various font-loaders (v0.1.1)
 - Added `theme_uv()` - a light mode theme based on theme_ipsum() (v0.1.2)
+- Fix namespacing bug (v0.1.3)
--- R/theme_tantastic.R
+++ R/theme_tantastic.R
@@ -217,33 +217,3 @@ theme_tantastic <- function(base_family = "IBM Plex Sans Condensed",
 
   ret
 }
-
-#' Update geom defaults
-#'
-#' @param colour colour of geom default
-#'
-set_geom_colour_defaults <- function(colour) {
-  geoms <- c(
-    "abline", "area", "bar", "boxplot", "col", "crossbar",
-    "density", "dotplot", "errorbar", "errorbar",
-    "hline", "label", "line", "linerange",
-    "map", "path", "point", "polygon", "rect", "ribbon", "rug", "segment",
-    "step", "text", "tile", "violin", "vline"
-  )
-
-  for (g in geoms) {
-    ggplot2::update_geom_defaults(g, list(colour = colour, fill = colour))
-  }
-}
-
-#' Update matching font defaults for text geoms
-#'
-#' Updates [ggplot2::geom_label] and [ggplot2::geom_text] font defaults
-#'
-#' @param family,face,size,color font family name, face, size and color
-#' @export
-set_geom_font_defaults <- function(family="IBM Plex Sans", face="plain", size=3.5,
-                                      color = "#2b2b2b") {
-  update_geom_defaults("text", list(family=family, face=face, size=size, color=color))
-  update_geom_defaults("label", list(family=family, face=face, size=size, color=color))
-}
