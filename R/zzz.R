1481435afccb4e1bfce9097b48a822dcee22af9f
Tan Ho <38083823+tanho63@users.noreply.github.com>
2021-09-08 23:29:12 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname,pkgname){
 
   if(interactive() && requireNamespace("extrafont",quietly = TRUE)){
-    fontimport_plex_condensed()
-    fontimport_bai_jamjuree()
+    # fontimport_plex_condensed()
+    # fontimport_bai_jamjuree()
     # fontimport_plex_condensed()
     # fontimport_jost()
   }
