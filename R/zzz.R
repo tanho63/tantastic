1f38eaec1c2d6d1fd22bb53c603a1d57c27cdb04
Tan Ho <tan@tanho.ca>
2023-03-12 22:32:44 -0400

    0.1.5 - add new shiny tz functions and move deps to suggests

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(libname,pkgname){
 
-  if(requireNamespace("extrafont",quietly = TRUE)){
-    fontimport_plex_condensed()
-    fontimport_bai_jamjuree()
+  # if(requireNamespace("extrafont",quietly = TRUE)){
+    # fontimport_plex_condensed()
+    # fontimport_bai_jamjuree()
     # fontimport_plex_condensed()
     # fontimport_jost()
-  }
+  # }
 
 }
